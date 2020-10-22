53add1a9867bc10a8757ed55aa117db81f4d4769
Jacob Barzee <jmbarzee@gmail.com>
2020-10-22 12:40:07 -0600

    updated pbconvert package to fully utilize new deviceNode interface

--- lightorchestrator/service/pbconvert/device_node.go
+++ lightorchestrator/service/pbconvert/device_node.go
@@ -3,26 +3,22 @@ package pbconvert
 import (
 	pb "github.com/jmbarzee/services/lightorchestrator/grpc"
 	"github.com/jmbarzee/services/lightorchestrator/service/device"
-	"github.com/jmbarzee/services/lightorchestrator/service/device/neopixel"
 )
 
 func NewPBDeviceNode(node device.DeviceNode) *pb.DeviceNode {
 	pbNode := &pb.DeviceNode{
-		UUID: node.GetID(),
-	}
-	switch n := node.(type) {
-	case *device.Group:
-		pbNode.Type = "Group"
-		for _, child := range n.DeviceNodes {
-			pbNode.Children = append(pbNode.Children, NewPBDeviceNode(child))
-		}
-	case device.GroupOption:
-		pbNode.Type = "GroupOption"
-		for _, child := range n.Groups {
-			pbNode.Children = append(pbNode.Children, NewPBDeviceNode(child))
-		}
-	case neopixel.Bar:
-		pbNode.Type = "npBar"
+		UUID:     node.GetID(),
+		Type:     node.GetType(),
+		Children: NewPBDeviceNodeList(node.GetChildren()),
 	}
 	return pbNode
+}
+
+func NewPBDeviceNodeList(nodes []device.DeviceNode) []*pb.DeviceNode {
+	pbNodes := make([]*pb.DeviceNode, len(nodes))
+
+	for i, node := range nodes {
+		pbNodes[i] = NewPBDeviceNode(node)
+	}
+	return pbNodes
 }
